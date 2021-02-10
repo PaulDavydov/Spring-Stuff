812a8b6d6ea564acaa4472dde9c9e2ac48ff203a
Paul Davydov <sirruskilotpaul@gmail.com>
2021-02-09 20:57:33 -0800

    My Spring projcets

---
+++ Payroll/src/main/java/com/example/restservices/Employee.java
@@ -0,0 +1,61 @@
+package com.example.restservices;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+class Employee {
+
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String role;
+
+    public Employee() {}
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public String getRole() {
+        return this.role;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setRole(String Role) {
+        this.role = role;
+    }
+    public boolean equals(Object o) {
+        if (this==o) {
+            return true;
+        }
+        if(!(o instanceof Employee)) {
+            return false;
+        }
+        Employee employee = (Employee) o;
+        return Objects.equals(this.id,employee.id) && Objects.equals(this.name,employee.name) &&
+                Objects.equals(this.role,employee.role);
+    }
+
+    public int hashCode() {
+        return Objects.hash(this.id,this.name,this.role);
+    }
+
+    public String toString() {
+        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' +
+                ", role='" + this.role + '\'' + '}';
+    }
+}
---
+++ Payroll/src/main/java/com/example/restservices/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.example.restservices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
